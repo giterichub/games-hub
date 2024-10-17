a5a81be1f2eea3830dc8ced1e1ee5b7f7e10ee2a
eric antao <ericantao99@gmail.com>
2024-10-17 20:18:44 +0530

    Removed text

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from '../assets/logo.webp';
+const Navbar = () => {
+    return (
+        <HStack>
+            <Image src={logo} boxSize='60px' />
+        </HStack>
+    );
+}
+export default Navbar;

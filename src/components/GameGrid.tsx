f58d9ef8756642e7aef9955044193c57b9e84200
eric antao <ericantao99@gmail.com>
2024-10-22 19:36:29 +0530

    Generic Data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-    const { games, error, isLoading } = useGames();
+    const { data, error, isLoading } = useGames();
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
     
     // console.log(games);
@@ -15,14 +15,14 @@ const GameGrid = () => {
         <>
             
             { error && <Text> {error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={10} spacing={10}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 3 }} padding='10px' spacing={10}>
             { isLoading && skeletons.map(skeleton => (
                 <GameCardContainer>
                     <GameCardSkeleton key={skeleton} />
                 </GameCardContainer>
             
             )) }
-                    {games.map(game => (
+                    {data.map(game => (
                         <GameCardContainer>
                             <GameCard key={game.id} game={game} />
                         </GameCardContainer>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,11 @@
-import useGenre from "../hooks/useGenre";
+import useGenre, { Genre } from "../hooks/useGenre";
 
 const GenreList = () => {
-    const { genres } = useGenre();
+    const {data} = useGenre();
     return (
-        <ul> {genres.map(genre => <li key={genre.id}> {genre.name} </li> )} </ul>
+        <ul> 
+            {data.map(genre => <li key={genre.id}> {genre.name} </li> )} 
+        </ul>
     );
 }
 export default GenreList;

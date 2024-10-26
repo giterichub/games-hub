5c0b239623a4aa493e37bdadd8014fb72a9ffd71
eric antao <ericantao99@gmail.com>
2024-10-26 19:30:26 +0530

    Building an input search

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -3,7 +3,7 @@ import { HStack, Switch, useColorMode, Text } from "@chakra-ui/react";
 const ColorModeSwitch = () => {
     const { toggleColorMode, colorMode } = useColorMode();
     return (
-        <HStack>
+        <HStack whiteSpace='nowrap'>
             <Switch colorScheme="green" isChecked={colorMode === 'dark'} onChange={toggleColorMode} ></Switch>
             <Text>Dark Mode</Text>
         </HStack>
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,8 @@
+import { Input } from "@chakra-ui/react";
+
+const SearchInput = () => {
+    return (
+        <Input borderRadius={20} variant='filled' placeholder='Search games...' />
+    );
+}
+export default SearchInput;

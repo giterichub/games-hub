cf5fe1c315b7b7be8ab6bd52464b982fd112e661
eric antao <ericantao99@gmail.com>
2024-10-29 12:15:33 +0530

    Refactoring Gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,11 +16,8 @@ const GameGrid = ({gameQuery}: Props) => {
     
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
         // console.log(games.map(game => console.log(game)));
-
+    if (error) return <Text> {error}</Text>;
     return (
-        <>
-            
-            { error && <Text> {error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 3 }} padding='10px' spacing={5}>
             { isLoading && skeletons.map(skeleton => (
                 <GameCardContainer key={skeleton}>
@@ -35,7 +32,6 @@ const GameGrid = ({gameQuery}: Props) => {
                     ) )}
                 
             </SimpleGrid>
-        </>
     );
 }
 export default GameGrid;

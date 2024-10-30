c9deb33541fb09b14ef8d5ceab2bd305029b3516
eric antao <ericantao99@gmail.com>
2024-10-31 00:16:43 +0530

    App updated with hamburger menu in Navbar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { Box, Grid, GridItem, HStack, Show, Drawer, DrawerOverlay, DrawerContent, DrawerHeader, 
-  DrawerBody, Button, useDisclosure, useBreakpointValue, IconButton } 
+import { Box, Grid, GridItem, HStack, Show } 
 from '@chakra-ui/react'
-import { FaHamburger } from 'react-icons/fa';
-import { MdClose } from "react-icons/md";
 import Navbar from './components/Navbar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
@@ -12,7 +9,6 @@ import PlatformSelector from './components/PlatformSelector';
 import { Platform } from './hooks/useGames';
 import SortSelector from './components/SortSelector';
 import GameHeading from './components/GameHeading';
-import theme from './theme';
 
 export interface GameQuery{
   genre: Genre | null;
@@ -23,7 +19,6 @@ export interface GameQuery{
 
 function App() {
   const [ gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery); 
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
@@ -37,17 +32,10 @@ function App() {
     }}
     transition="background-color 200ms linear"
     >
-      <Show below="lg">
-                    <IconButton boxSize='30px' marginLeft={3}
-                        aria-label="Open Menu"
-                        icon={<FaHamburger />}
-                        onClick={onOpen}
-                        variant="outline"
-                        marginY={4}
-                    />
-                </Show>
+
       <GridItem area={'nav'}>
-        <Navbar onSearch={(searchText) => setGameQuery({...gameQuery, searchText})} />
+        <Navbar  onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })} 
+            gameQuery={gameQuery} setGameQuery={setGameQuery}  />
       </GridItem>
       <Show above="lg">
         <GridItem area={'aside'} paddingX={5}><GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})} /></GridItem>
@@ -63,22 +51,6 @@ function App() {
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
-    <Drawer isOpen={isOpen} onClose={onClose} placement="left">
-    <DrawerOverlay>
-        <DrawerContent>
-          <HStack justifyContent='space-between' paddingRight={3}>
-            <DrawerHeader>Menu</DrawerHeader>
-            <MdClose size='25px' onClick={onClose} />
-          </HStack>
-            <DrawerBody>
-                {/* Add your menu items here */}
-                <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => {setGameQuery({...gameQuery, genre}); onClose()}} />
-                {/* <Button  w="100%">Close</Button> */}
-                {/* You can add more buttons or links as needed */}
-            </DrawerBody>
-        </DrawerContent>
-    </DrawerOverlay>
-</Drawer>
 </>
   )
 }

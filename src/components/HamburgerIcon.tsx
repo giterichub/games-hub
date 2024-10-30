392929db77ea85dca82a478b775cd53ebec0bb66
eric antao <ericantao99@gmail.com>
2024-10-31 00:13:04 +0530

    Hamburger Icon Developed

---
+++ src/components/HamburgerIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { IconButton, useDisclosure } from '@chakra-ui/react';
+import { FaHamburger } from 'react-icons/fa';
+
+interface Props{
+    onOpen: () => void;
+}
+
+const HamburgerIconComponent = ({ onOpen }: Props) => {
+    return (
+        <IconButton
+            aria-label="Open Menu"
+            icon={<FaHamburger />}
+            onClick={onOpen}
+            variant="outline"
+        />
+    );
+};
+
+export default HamburgerIconComponent;

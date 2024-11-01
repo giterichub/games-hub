61a8adce3dc2c56df9e15110aad3144a833f0f41
eric antao <ericantao99@gmail.com>
2024-11-01 12:40:03 +0530

    Emoji hover title text

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,4 +1,4 @@
-import { Image, ImageProps } from "@chakra-ui/react";
+import { Image, ImageProps, Tooltip } from "@chakra-ui/react";
 import bullsEye from "../assets/bulls-eye.webp"
 import thumbsUp from "../assets/thumbs-up.webp"
 import meh from "../assets/meh.webp"
@@ -13,9 +13,11 @@ const Emoji = ({rating}: Props) => {
         4: { src: thumbsUp, alt: 'recommended', boxSize: '25px' },
         5: { src: bullsEye, alt: 'Exceptional', boxSize: '32px' }
     }
-
+    
     return (
-        <Image {...emojiMap[rating]} marginTop={1} />
+        <Tooltip label={{...emojiMap[rating]}.alt} fontSize='md'>
+            <Image {...emojiMap[rating]} marginTop={1} />
+        </Tooltip>
     );
 }
 export default Emoji;
